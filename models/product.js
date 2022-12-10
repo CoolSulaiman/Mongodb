347b94162643b776065f1b6cc030dc5097a6a478
Sulaiman <sulaimanchapti786@gmail.com>
2022-12-10 09:14:06 +0530

    Crud operations

--- models/product.js
+++ models/product.js
@@ -1,16 +1,26 @@
+const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb ; 
 
 class Product {
-  constructor(title ,price ,imageUrl ,description ){
+  constructor(title ,price ,imageUrl ,description , id ){
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
-    this.description = description
+    this.description = description;
+    this._id=id;
   }
 
   save(){
     const db = getDb();
-    return db.collection('products').insertOne(this)
+    let dbOp;
+    if(this._id){
+      //Updaate the product
+      dbOp = db.collection('products')
+      .updateOne({_id : new mongodb.ObjectId(this._id) }, {$set:this})
+    }else{
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
     .then(result=>{
       console.log(result,"jjj")
     })
@@ -18,6 +28,51 @@ class Product {
       console.log(err)
     })
   }
+
+
+static fetchAll() {
+
+  let db = getDb();
+
+  return db
+  .collection('products')
+  .find()
+  .toArray()
+  .then(products=>{
+    // console.log(products)
+    return products
+  })
+  .catch(err=>{
+    console.log(err)
+  })
 }
+
+
+static findById(proId) {
+  const db = getDb();
+  return db.collection('products')
+  .find({_id:new mongodb.ObjectId(proId)})
+  .next()
+  .then(product =>{
+    console.log(product);
+    return product ;
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+
+static deleteById(prodId) {
+  const db = getDb();
+  return db.collection("products").deleteOne({_id : new mongodb.ObjectId(prodId) })
+  .then(product =>{
+    console.log(" Product Deleted");
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+}
+
 
 module.exports = Product;

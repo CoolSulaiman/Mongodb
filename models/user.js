e9e117eec50c6aad2d1040a43974a1797010e1a0
Sulaiman <sulaimanchapti786@gmail.com>
2022-12-10 09:32:19 +0530

    Added User model

--- models/user.js
+++ models/user.js
@@ -1,16 +1,26 @@
-const Sequelize = require('sequelize');
 
-const sequelize = require('../util/database');
+const mongodb = require('mongodb')
+const getDb = require('../util/database').getDb ; 
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(name , email){
+    this.name = name;
+    this.email = email 
+  }
 
-module.exports = User;
+static findById(userId){
+  let db = getDb();
+  return db.collection('users').findOne({_id: new mongodb.ObjectId(userId)})
+  .then(user=>{
+    return user
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+}
+
+module.exports = User
+
+
+
